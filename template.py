4016ee952ad675625ddbf307813ae3758fffece4
akinolanath <akinolanathaniel3026>
2024-06-22 16:32:40 -0500

    updated_folder

--- template.py
+++ template.py
@@ -20,7 +20,7 @@ list_of_files = [
     "src/utils/__init__.py",
     "src/utils/utils.py",
     "src/logger/logging.py",
-    "src/exception3q/exception",
+    "src/exception/exception.py",
     "tests/unit/__init__.py",
     "tests/integration/__init__.py", 
     "init_setup.sh",
